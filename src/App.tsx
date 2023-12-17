ba1a849aa03543c66d47c1be2c1ddc536321d963
Kruthibharath <kruthi.bharath@gmail.com>
2023-12-17 23:17:14 +0000

    feat:added submit button eventhandler to capture all the form data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import W12MForm from "./components/W12MForm";
 
+interface AppFormData {
+  speciesName: string;
+  planetName: string;
+  numberOfBeings: number;
+  addNumbers: string;
+  reason: string;
+}
+
 function App() {
+  const [formEntries, setFormEntries] = useState<AppFormData | null>(null);
+  const updateAppFormData = (data: AppFormData) => {
+    setFormEntries(data);
+  };
   return (
     <>
       <h1>W-12-M :- APPLICATION TO SPARE PLANET FROM DESTRUCTION</h1>
-      <W12MForm />
+      <W12MForm updateAppFormData={updateAppFormData} />
+      <p>FormData: {JSON.stringify(formEntries)}</p>
     </>
   );
 }
--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -6,35 +6,68 @@ import { NumberOfBeings } from "./NumberOfBeings";
 import { AddNumbers } from "./AddNumbers";
 import { ReasonForSparing } from "./ReasonForSparing";
 
-const W12MForm = () => {
+interface AppFormData {
+  speciesName: string;
+  planetName: string;
+  numberOfBeings: number;
+  addNumbers: string;
+  reason: string;
+}
+interface WebFormProps {
+  updateAppFormData: (data: AppFormData) => void;
+}
+
+const W12MForm: React.FC<WebFormProps> = ({ updateAppFormData }) => {
   const [speciesName, setSpeciesName] = useState("Humans");
   const [planetName, setPlanetName] = useState("Earth");
   const [numberOfBeings, setNumberOfBeings] = useState(0);
   const [addNumbers, setAddNumbers] = useState("Please select your answer");
   const [reason, setReason] = useState("");
+  /*const [formData, setFormData] = useState<AppFormData>({
+    speciesName: "",
+    planetName: "",
+    numberOfBeings: 0,
+    addNumbers: "",
+    reason: "",
+  });*/
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData: AppFormData = {
+      speciesName,
+      planetName,
+      numberOfBeings,
+      addNumbers,
+      reason,
+    };
+    updateAppFormData(formData);
+    console.log(formData);
+  };
   return (
     <section className="w12MForm">
       <W12MHeader />
-      <SpeciesName
-        speciesName={speciesName}
-        onChangeSpeciesName={(value) => setSpeciesName(value)}
-      />
-      <PlanetName
-        planetName={planetName}
-        onChangePlanetName={(value) => setPlanetName(value)}
-      />
-      <NumberOfBeings
-        numberOfBeings={numberOfBeings}
-        onChangeNumberOfBeings={(value) => setNumberOfBeings(value)}
-      />
-      <AddNumbers
-        result={addNumbers}
-        onChangeOfSelection={(value) => setAddNumbers(value)}
-      />
-      <ReasonForSparing
-        reason={reason}
-        onChangeOfReason={(value) => setReason(value)}
-      />
+      <form onSubmit={handleSubmit}>
+        <SpeciesName
+          speciesName={speciesName}
+          onChangeSpeciesName={(value) => setSpeciesName(value)}
+        />
+        <PlanetName
+          planetName={planetName}
+          onChangePlanetName={(value) => setPlanetName(value)}
+        />
+        <NumberOfBeings
+          numberOfBeings={numberOfBeings}
+          onChangeNumberOfBeings={(value) => setNumberOfBeings(value)}
+        />
+        <AddNumbers
+          result={addNumbers}
+          onChangeOfSelection={(value) => setAddNumbers(value)}
+        />
+        <ReasonForSparing
+          reason={reason}
+          onChangeOfReason={(value) => setReason(value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </section>
   );
 };
