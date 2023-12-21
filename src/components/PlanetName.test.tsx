20e761b2c4c7b3039985f6a05dfcc74794d855a2
Kruthibharath <kruthi.bharath@gmail.com>
2023-12-21 20:07:02 +0000

    feat: added test cases similar to species name input field to planet input field

--- src/components/PlanetName.test.tsx
+++ src/components/PlanetName.test.tsx
@@ -1,8 +1,18 @@
-import { render } from "@testing-library/react"
-import { PlanetName } from "./PlanetName"
+import { render, screen } from "@testing-library/react";
+import { PlanetName } from "./PlanetName";
+import { resolveModuleNameFromCache } from "typescript";
 
-describe("Planet Component",()=>{
-    test("component renders without fail",()=>{
-        render(<PlanetName planetName={} onChangePlanetName={()=>{}})
-    })
-})
+describe("Planet Component", () => {
+  test("component renders without fail", () => {
+    render(<PlanetName planetName={"Earth"} onChangePlanetName={() => {}} />);
+  });
+
+  test("components renders with different props", () => {
+    const { rerender } = render(
+      <PlanetName planetName="Earth" onChangePlanetName={() => {}} />
+    );
+    rerender(<PlanetName planetName="Mars" onChangePlanetName={() => {}} />);
+    const inputValueForPlanet = screen.getByDisplayValue("Mars");
+    expect(inputValueForPlanet).toBeInTheDocument();
+  });
+});
