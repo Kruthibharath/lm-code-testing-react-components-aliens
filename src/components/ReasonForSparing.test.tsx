3984852bd8b48b9ce7dd726728e36f806e4ed146
Kruthibharath <kruthi.bharath@gmail.com>
2023-12-23 18:37:06 +0000

    feat: added test suits for reasonforsparing component

--- src/components/ReasonForSparing.test.tsx
+++ src/components/ReasonForSparing.test.tsx
@@ -1,3 +1,14 @@
-describe("Reason for Sparing",()=>{
-    test("")
-})
+import { render, screen } from "@testing-library/react";
+import { ReasonForSparing } from "./ReasonForSparing";
+
+describe("Reason for Sparing", () => {
+  test("renders the component without fail", () => {
+    render(<ReasonForSparing reason="" onChangeOfReason={() => {}} />);
+  });
+
+  test("renders textarea with the initial values", () => {
+    render(<ReasonForSparing reason="" onChangeOfReason={() => {}} />);
+    const textareaElement = screen.getByLabelText("Reason for sparing");
+    expect(textareaElement).toBeInTheDocument();
+  });
+});
--- src/components/ReasonForSparing.tsx
+++ src/components/ReasonForSparing.tsx
@@ -10,6 +10,7 @@ export const ReasonForSparing: React.FC<reasonForSparingProps> = ({
     <div>
       <label htmlFor="reason">Reason for sparing</label>
       <textarea
+        id="reason"
         value={reason}
         onChange={(e) => onChangeOfReason(e.target.value)}
         title="Enter the reason for sparing"
